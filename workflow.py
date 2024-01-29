670b39e77c36b5e100fe5a9fac4dd600a3a3f949
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-01-29 20:03:41 +0100

    Prepared setup for cluster

--- workflow.py
+++ workflow.py
@@ -1,10 +1,11 @@
 
+import os
 from gwf import Workflow, AnonymousTarget
 
 ### EXPERIMENTS ###
 def make_experiment_data(): 
-    inputs = ['data/RNAStralign.tar.gz']
-    outputs = ['data/experiment.pkl']
+    inputs = [os.path.join('data', 'RNAStralign.tar.gz')]
+    outputs = [os.path.join('data', 'experiment.pkl')]
     options = {"memory":"32gb", "walltime":"03:00:00"}
     spec = """python3 scripts/experiment_files.py"""
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
