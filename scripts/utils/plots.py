d646e41444948c7601d00d668fe718dfc8e07119
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-01-30 13:55:23 +0100

    Removed unused parameters

---
+++ scripts/utils/plots.py
@@ -0,0 +1,76 @@
+import os, pickle
+import matplotlib as plt
+import numpy as np
+
+def plot_families(file_dict, output_file = None):
+  file_dict = {key:item for key, item in file_dict.items() if len(item)>0}
+
+  file_lists = [item for key, item in file_dict.items()]
+  data_set = [key for key, item in file_dict.items()]
+
+  RNA_families = {family: [0]*len(file_lists) for family in ['16SrRNA', '5SrRNA', 'RNaseP', 'SRP', 'groupIintron', 'tRNA','telomerase', 'tmRNA']}
+
+  for index, file_list in enumerate(file_lists):
+    for file in file_list:
+      family = pickle.load(open(file, 'rb')).family
+      RNA_families[family][index] += 1
+
+  RNA_families = sorted({family:item for family, item in RNA_families.items() if item != ([0]*len(file_lists))}.items(), key=lambda x:x[1])
+  values = [[f[1][i]/len(file_lists[i]) for i in range(len(file_lists))] for f in RNA_families]
+  families = [f[0] for f in RNA_families]
+
+  x = np.arange(len(families))
+  width = 1/len(file_dict)*0.9
+
+  fig, ax = plt.subplots(figsize=(10,6))
+  for index in range(len(file_lists)):
+    offset = width*index
+    ax.bar(x+offset, [v[index] for v in values], width=width, edgecolor = 'black', label = data_set[index])
+
+  ax.grid(axis = 'y', linestyle = '--')
+  ax.set_axisbelow(True)
+  ax.set_xticks(x + width / 2)
+  ax.set_xticklabels(families, rotation = 45, ha='right')
+  ax.legend(loc='center left', bbox_to_anchor=(1, 0.94))
+
+  ax.set_ylabel("Density")
+  ax.set_title('Distribution of data (families)')
+
+  plt.tight_layout()
+
+  if output_file:
+    plt.savefig(output_file, bbox_inches = 'tight')
+
+  plt.show()
+
+
+def plot_len_histogram(file_dict, output_file = None):
+  file_dict = {key:item for key, item in file_dict.items() if len(item)>0}
+
+  file_lists = [item for key, item in file_dict.items()]
+  data_set = [key for key, item in file_dict.items()]
+
+  # Takes a list of .ct file and produces a histogram of the distribution of lengths of sequences
+  lengths = []
+
+  for file_list in file_lists:
+    l = []
+    for file in file_list:
+      l.append(pickle.load(open(file, 'rb')).length)
+    lengths.append(l)
+
+  plt.figure(figsize=(10,6))
+
+  plt.hist(lengths, bins=50, edgecolor='black', label = data_set, zorder = 3, density = True)
+
+  plt.title('Distribution of data (lengths)')
+  plt.xlabel('Sequence Length')
+  plt.ylabel("Density")
+  plt.legend(loc='center left', bbox_to_anchor=(1, 0.94))
+  plt.grid(axis = 'y', linestyle='--', zorder=0)
+  plt.tight_layout()
+
+  if output_file:
+    plt.savefig(output_file, bbox_inches = 'tight')
+
+  plt.show()
