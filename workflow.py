5e39a25cce5b9508e30e7ed5ca94737735ada852
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-01-30 10:40:52 +0100

    Changed to accomodate to matrix types

--- workflow.py
+++ workflow.py
@@ -5,7 +5,7 @@ from gwf import Workflow, AnonymousTarget
 ### EXPERIMENTS ###
 def make_experiment_data(matrix_type): 
     inputs = [os.path.join('data', 'RNAStralign.tar.gz')]
-    outputs = [os.path.join('data', f'experiment{matrix_type}.pkl')]
+    outputs = [os.path.join('data', f'experiment{matrix_type}.pkl.gz')]
     options = {"memory":"64gb", "walltime":"03:00:00"}
     spec = """python3 scripts/experiment_files.py {matrix_type}
     gzip data/experiment{matrix_type}.pkl""".format(matrix_type = matrix_type)
