ae748632867901bcb90a9e77c1eead98886acab1
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-02-01 13:47:19 +0100

    Moved function back into file

--- workflow.py
+++ workflow.py
@@ -8,7 +8,7 @@ def make_experiment_data(matrix_type):
     outputs = [os.path.join('data', f'experiment{matrix_type}.tar.gz')]
     options = {"memory":"16gb", "walltime":"03:00:00"}
     spec = """python3 scripts/experiment_files.py {matrix_type}
-    tar -czvf data/experiment{matrix_type}.tar.gz data/experiment{matrix_type}
+    tar -czf data/experiment{matrix_type}.tar.gz data/experiment{matrix_type}
     rm -r data/experiment{matrix_type}""".format(matrix_type = matrix_type)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
