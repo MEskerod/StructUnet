3853b4d16646c9f2b4da91feaabe3a3c5d7584dc
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-03-05 13:30:59 +0100

    Initial commit

---
+++ scripts/utils/hotknots.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Node: 
+    def __init__(self) -> None:
+        pass
+
+class Tree:
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __iter__(self):
+        pass
+
+def initialize_tree(sequence):
+    return Tree()
+
+
+
+def hotknots(sequence):
+    return
