44e97cc8d86196a8c46c2dc2b8655f3af2d6fff4
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-03-13 21:22:12 +0100

    Corrected ratio for split and output of target

--- workflow.py
+++ workflow.py
@@ -6,7 +6,7 @@ from gwf import Workflow, AnonymousTarget
 def make_experiment_data(matrix_type): 
     inputs = [os.path.join('data', 'RNAStralign.tar.gz')]
     outputs = [os.path.join('data', f'experiment{matrix_type}.tar.gz')]
-    options = {"memory":"16gb", "walltime":"03:00:00"}
+    options = {"memory":"16gb", "walltime":"03:00:00", "account":"RNA_Unet"}
     spec = """python3 scripts/experiment_files.py {matrix_type}
     tar -czf data/experiment{matrix_type}.tar.gz data/experiment{matrix_type}
     rm -r data/experiment{matrix_type}""".format(matrix_type = matrix_type)
@@ -16,7 +16,7 @@ def postprocess_time():
     inputs = []
     outputs = [os.path.join('results', 'postprocess_time.csv'),
                os.path.join('figures', 'postprocess_time.png')]
-    options = {"memory": "16gb", "walltime": "36:00:00"}
+    options = {"memory": "16gb", "walltime": "36:00:00", "account":"RNA_Unet", "cores": 4}
     spec = """python3 scripts/time_postprocessing.py"""
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
@@ -24,7 +24,7 @@ def convert_time():
     inputs = []
     outputs = [os.path.join('results', 'convert_time.csv'),
                os.path.join('figures', 'convert_time.png')]
-    options = {"memory": "16gb", "walltime": "24:00:00"}
+    options = {"memory": "16gb", "walltime": "24:00:00", "account":"RNA_Unet"}
     spec = """python3 scripts/time_matrix_conversion.py"""
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
@@ -37,10 +37,9 @@ def make_complete_set():
                os.path.join('data', 'train.pkl'),
                os.path.join('data', 'valid.pkl'),
                os.path.join('data', 'test.pkl'),
-               os.path.join('data', 'familymap.pkl'),
                os.path.join('figures', 'length_distribution.png'),
                os.path.join('figures', 'family_distribution.png')]
-    options = {"memory":"16gb", "walltime":"48:00:00"}
+    options = {"memory":"16gb", "walltime":"48:00:00", "account":"RNA_Unet", "cores": 4}
     spec = """python3 scripts/complete_dataset.py
     tar -czf data/test_files.tar.gz data/test_files
     rm -r data/test_files
@@ -54,7 +53,7 @@ def hyper_parametersearch():
               os.path.join('data', 'train.pkl'),
               os.path.join('data', 'valid.pkl')]
     outputs = ['hyperparameter_log.txt']
-    options = {"memory":"64gb", "walltime":"72:00:00"} #NOTE - Think about memory
+    options = {"memory":"64gb", "walltime":"72:00:00", "account":"RNA_Unet"} #NOTE - Think about memory
     spec = """mdir data/complete_set
     tar -xzf data/complete_set.tar.gz -C data/complete_set
     python3 scripts/hyperparameter_search.py
@@ -64,7 +63,7 @@ def hyper_parametersearch():
 def train_model(): 
     inputs = []
     outputs = []
-    options = {"memory":"16gb", "walltime":"72:00:00"} #NOTE - Think about memory and walltime
+    options = {"memory":"16gb", "walltime":"72:00:00", "account":"RNA_Unet"} #NOTE - Think about memory and walltime
     spec = """mdir data/complete_set
     tar -xzf data/complete_set.tar.gz -C data/complete_set
     python3 scripts/train_model.py
@@ -75,7 +74,7 @@ def evaluate_nn():
     inputs = [os.path.join('data', 'test_files.tar.gz')] #FIXME - Add path to model
     outputs = [os.path.join('results', 'evaluation_nn.csv'),
                os.path.join('figures', 'evaluation_nn.png')]
-    options = {"memory":"16gb", "walltime":"24:00:00"} #NOTE - Think about memory and walltime
+    options = {"memory":"16gb", "walltime":"24:00:00", "account":"RNA_Unet"} #NOTE - Think about memory and walltime
     spec = """tar -xzf data/test_files.tar.gz -C data/test_files
     python3 scripts/evaluate_nn.py
     rm -r data/test_files"""
