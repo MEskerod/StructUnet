e6d932439ae2eaa7eecbc9bd83ab9205ca49c0a9
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-03-22 14:26:30 +0100

    Test workflow

--- workflow.py
+++ workflow.py
@@ -93,5 +93,6 @@ gwf.target_from_template('train_model', train_model())
 
 #Predicting with other methods for comparison
 test_files = pickle.load(open('data/test.pkl', 'rb'))
-for i, file in enumerate(test_files[0]): 
+test_files = test_files[:1]
+for i, file in enumerate(test_files): 
     gwf.target_from_template(f'predict_hotknots_file_no_{i}', predict_hotknots(file))
