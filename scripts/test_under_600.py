03865e69855f7a6ef3c678ec413a161071a47055
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-04-03 11:23:21 +0200

    Initial commit

--- scripts/test_under_600.py
+++ scripts/test_under_600.py
@@ -1,4 +1,5 @@
 import pickle
+from tqdm import tqdm
 from collections import namedtuple
 
 
@@ -11,10 +12,19 @@ if __name__ == '__main__':
 
     train = pickle.load(open('data/train.pkl', 'rb'))
 
+    print(f"Total files: {len(train)}")
+    print("Finding files under 600")
+
+    progress = tqdm.tqdm(total=len(train), unit = 'files')
+    
     for i, file in enumerate(train): 
         length = pickle.load(open(file, 'rb')).length
         if length < 600: 
             files_idx.append(i)
+        
+        progress.update(1)
+        
+    progress.close()
     
 
     print(f"Files under 600: {len(files_idx)}")
