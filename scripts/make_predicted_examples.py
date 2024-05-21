5dddc3f986cc3a11b28925b57e484bb71254ad21
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-05-21 11:36:12 +0200

    Corrected errors

--- scripts/make_predicted_examples.py
+++ scripts/make_predicted_examples.py
@@ -5,6 +5,8 @@ from collections import namedtuple
 import numpy as np
 import pandas as pd
 
+from tqdm import tqdm
+
 
 
 from utils.model_and_training import RNA_Unet, evaluate
@@ -34,11 +36,13 @@ def output_to_bpseq(output: torch.Tensor, sequence: str, name:str) -> str:
     """
     pairs = torch.nonzero(output)
 
-    bpseq = [f'Filename: {name}'] + [[str(i+1), sequence[i], str(0)] for i in range(len(sequence))]
+    bpseq = [[str(i+1), sequence[i], str(0)] for i in range(len(sequence))]
 
     for row, col in pairs: 
         if not row == col:
             bpseq[row][2] = str(col.item()+1)
+    
+    bpseq = [f'Filename: {name}'] + bpseq
 
     return '\n'.join([' '.join(line) for line in bpseq])
 
@@ -51,13 +55,12 @@ def find_examples() -> dict:
 
     telomerase, RNAaseP, srp, tmRNA, group_I_intron, tRNA, fiveS_rRNA, sixteeenS_rRNA = False, False, False, False, False, False, False, False
     pseudoknot1, pseudoknot2, pseudoknot3 = False, False, False
-    twentythreeS_rRNA, group_II_intron = False
+    twentythreeS_rRNA, group_II_intron = False, False
 
-    align_elements = [telomerase, RNAaseP, srp, tmRNA, group_I_intron, tRNA, fiveS_rRNA, sixteeenS_rRNA, pseudoknot1, pseudoknot2] 
     align = pickle.load(open('data/test.pkl', 'rb'))
 
     i = 0
-    while all(not elem for elem in align_elements): 
+    while not all([telomerase, RNAaseP, srp, tmRNA, group_I_intron, tRNA, fiveS_rRNA, sixteeenS_rRNA, pseudoknot1, pseudoknot2] ): 
         #Shouldn't be used, but check to ensure infinity loop and out of range indexing
         if i == len(align):
             break
@@ -69,69 +72,76 @@ def find_examples() -> dict:
         i += 1 #Increment the counter to next iteration
 
         if not telomerase and family == 'telomerase':
-            results['telomerase'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['telomerase'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             telomerase = True
+            print('Found telomerase')
             continue
 
         if not RNAaseP and family == 'RNaseP':
-            results['RNAaseP'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['RNAaseP'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             RNAaseP = True
+            print('Found RNaseP')
             continue
 
         if not srp and family == 'SRP':
-            results['srp'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['srp'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             srp = True
+            print('Found SRP')
             continue
 
         if not tmRNA and family == 'tmRNA':
-            results['tmRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['tmRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             tmRNA = True
+            print('Found tmRNA')
             continue
 
         if not group_I_intron and family == 'group_I_intron':
-            results['group_I_intron'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['group_I_intron'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             group_I_intron = True
+            print('Found group I intron') 
             continue
 
         if not tRNA and family == 'tRNA':
-            results['tRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['tRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             tRNA = True
+            print('Found tRNA')
             continue
 
         if not fiveS_rRNA and family == '5S_rRNA':
-            results['5S_rRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['5S_rRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             fiveS_rRNA = True
+            print('Found 5S rRNA')
             continue
 
         if not sixteeenS_rRNA and family == '16S_rRNA':
-            results['16S_rRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['16S_rRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             sixteeenS_rRNA = True
+            print('Found 16S rRNA')
             continue
 
         #Check if the true structure has pseudoknots
         pk = has_pk(np.argmax(file.output, axis=1))
 
         if not pseudoknot1 and pk:
-            results['pseudoknot1'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['pseudoknot1'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             pseudoknot1 = True
+            print('Found first pseudoknot')
             continue
 
-        if not pseudoknot2 and pk:
-            results['pseudoknot2'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+        if not pseudoknot2 and pk and pseudoknot1 and family != results['pseudoknot1']['family']:
+            results['pseudoknot2'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             pseudoknot2 = True
+            print('Found second pseudoknot')
             continue
 
         i += 1
-        
+    
 
 
-
-
-    archive_elements = [pseudoknot3, twentythreeS_rRNA, group_II_intron]
     archive = pickle.load(open('data/archiveii.pkl', 'rb'))
 
     i = 0
-    while all(not elem for elem in archive_elements):
+    while not all([pseudoknot3, twentythreeS_rRNA, group_II_intron]):
         #Shouldn't be used, but check to ensure infinity loop and out of range indexing
         if i == len(archive):
             break
@@ -143,30 +153,39 @@ def find_examples() -> dict:
         i += 1 #Increment the counter to next iteration
 
         if not twentythreeS_rRNA and family == '23s':
-            results['23S_rRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['23S_rRNA'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             twentythreeS_rRNA = True
+            print('Found 23S rRNA')
             continue
 
         if not group_II_intron and family == 'grp2':
-            results['group_II_intron'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+            results['group_II_intron'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             group_II_intron = True
+            print('Found group II intron')
             continue
 
         #Check if the true structure has pseudoknots
         pk = has_pk(np.argmax(file.output, axis=1))
 
-        if not pseudoknot3 and pk:
-            results['pseudoknot3'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input}
+        if not pseudoknot3 and pk and pseudoknot1 and pseudoknot2 and family != results['pseudoknot1']['family'] and family != results['pseudoknot2']['family']:
+            results['pseudoknot3'] = {'sequence': file.sequence, 'output': file.output, 'name': file.name, 'input': file.input, 'family': family}
             pseudoknot3 = True
+            print('Found third pseudoknot')
             continue
 
     return results
 
 def predict(examples: dict) -> None: 
-    for example in examples: 
-        input = torch.tensor(example['input']).unsqueeze(0).to(device)
+
+    progress_bar = tqdm(total=len(examples), unit='example', desc='Predicting structures', file=sys.stdout)
+    
+    for _, item in examples.items():
+        input = item['input'].unsqueeze(0).to(device)
         output = model(input)
-        example['predicted'] = output.squeeze(0).squeeze(0).detach()
+        item['predicted'] = output.squeeze(0).squeeze(0).detach()
+        progress_bar.update(1)
+    
+    progress_bar.close()
     
 
 def save_examples(examples: dict) -> None:
@@ -190,10 +209,15 @@ def save_examples(examples: dict) -> None:
 
 def record_f1_scores(examples: dict) -> None:
     scores = []
+
+    progress_bar = tqdm(total=len(examples), unit='example', desc='Recording F1 scores', file=sys.stdout)
     
     for family, example in examples.items(): 
         precision, recall, F1 = evaluate(example['predicted'], example['output'], device=device)
-        scores.append({'example': family, 'length': len(example['sequence']), 'precision': precision, 'recall': recall, 'F1': F1})
+        scores.append({'example': family, 'family': example['family'], 'length': len(example['sequence']), 'precision': precision, 'recall': recall, 'F1': F1})
+        progress_bar.update(1)
+
+    progress_bar.close()
 
     
     df = pd.DataFrame(scores)
@@ -214,7 +238,7 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load('RNA_Unet.pth', map_location=torch.device(device)))
     model.to(device)
 
-    print('\nPredicting structures')
+    print('\n')
     predict(examples)
 
     print('\nSaving examples\n')
