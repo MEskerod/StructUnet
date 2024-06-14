e8f73d6857962358abcddf280ed8c34f0ddc75cf
MEskerod <112616513+MEskerod@users.noreply.github.com>
2024-06-14 11:20:43 +0200

    Updated plot for 16S rRNA

--- scripts/16SrRNA.py
+++ scripts/16SrRNA.py
@@ -1,12 +1,12 @@
 import pickle, torch, os
 
 import pandas as pd
-
-import tqdm as tqdm
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-from collections import namedtuple 
+from collections import namedtuple
+from scipy.stats import linregress 
+from tqdm import tqdm
 
 from utils.model_and_training import evaluate
 
@@ -24,19 +24,25 @@ def count_non_standard_bp(sequence, output):
     
     return non_standard
 
-def plot_correlation(non_std, lengths, f1s): 
-    fig, ax = plt.subplots(figsize=(8, 8))
+def plot_correlation(df): 
+    fig, ax = plt.subplots(figsize=(6, 5))
 
-    y = [i*2/j for i, j in zip(non_std, lengths)]
+    x = np.array([i*2/j for i, j in zip(df['Non-standard basepairs'], df['Length'])])
 
-    ax.scatter(y, f1s, s=10, alpha=0.5)
+    slope, intercept, r_value, p_value, std_err = linregress(x, df['F1 score'])
+    r_squared = r_value**2
+
+    ax.scatter(x, df['F1 score'], s=10, alpha=0.5)
+    ax.plot(x, intercept+slope*x, color='red', label = f'$y = {slope:.2f}x + {intercept:.2f}$\n$R^2$ = {r_squared:.2f}')
 
     ax.grid(linestyle = '--')
-    ax.set_xlabel("F1 score")
-    ax.set_ylabel("Proportion of bases in non-standard basepairs")
+    ax.set_ylabel("F1 Score")
+    ax.set_xlabel("Proportion of Bases in Non-Standard Basepairs")
 
     plt.gca().spines['right'].set_visible(False)
     plt.gca().spines['top'].set_visible(False)
+
+    plt.legend(loc='upper right', frameon=False)
 
     plt.tight_layout()
 
@@ -50,9 +56,9 @@ if __name__ == '__main__':
 
     RNA = namedtuple('RNA', 'input output length family name sequence')
 
-    df = pd.DataFrame(index = range(len(files)), columns=['Length', 'Non-standard basepairs', 'F1 score'])
+    df = pd.DataFrame(columns=['Length', 'Non-standard basepairs', 'F1 score'])
 
-    progress = tqdm.tqdm(total=len(files), unit='files')
+    progress = tqdm(total=len(files), unit='files')
 
     for i, file in enumerate(files):
         data = pickle.load(open(file, 'rb'))
@@ -65,7 +71,7 @@ if __name__ == '__main__':
         predicted = pickle.load(open(f'steps/RNA_Unet/{os.path.basename(file)}', 'rb'))
         _, _, F1_score = evaluate(predicted, data.output, device='cpu')
 
-        df.loc[i] = [len(data.sequence), non_std, F1_score]
+        df.loc[len(df)] = [len(data.sequence), non_std, F1_score]
 
         progress.update()
     
